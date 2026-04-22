72b5c6396bdba11dda4889a1dfac91fb13eced2e
bsc_com_ne_05_20 <bsc_com_ne_05_20@unima.ac.mw>
2026-04-22 06:13:14 +0200

    doing something: executing a command on the remote host

--- Typical_ssh_session/Main.c
+++ Typical_ssh_session/Main.c
@@ -48,6 +48,15 @@ int main(){
         exit(-1);
     }
 
+    //execute a command on the remote server and print the output.
+    if (show_remote_processes(my_ssh_session) != SSH_OK) {
+        fprintf(stderr, "Error executing remote command: %s\n",
+                ssh_get_error(my_ssh_session));
+        ssh_disconnect(my_ssh_session);
+        ssh_free(my_ssh_session);
+        exit(-1);
+    }
+
 
     ssh_disconnect(my_ssh_session);
     ssh_free(my_ssh_session);
@@ -143,4 +152,58 @@ int verify_knownhost(ssh_session session)
  
     ssh_clean_pubkey_hash(&hash);
     return 0;
+}
+
+//execeute a command on the remote server and print the output.
+//Doing something example
+int show_remote_processes(ssh_session session)
+{
+  ssh_channel channel = NULL;
+  int rc;
+  char buffer[256];
+  int nbytes;
+ 
+  channel = ssh_channel_new(session);
+  if (channel == NULL)
+    return SSH_ERROR;
+ 
+  rc = ssh_channel_open_session(channel);
+  if (rc != SSH_OK)
+  {
+    ssh_channel_free(channel);
+    return rc;
+  }
+ 
+  rc = ssh_channel_request_exec(channel, "ps aux");
+  if (rc != SSH_OK)
+  {
+    ssh_channel_close(channel);
+    ssh_channel_free(channel);
+    return rc;
+  }
+ 
+  nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
+  while (nbytes > 0)
+  {
+    if (write(1, buffer, nbytes) != (unsigned int) nbytes)
+    {
+      ssh_channel_close(channel);
+      ssh_channel_free(channel);
+      return SSH_ERROR;
+    }
+    nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
+  }
+ 
+  if (nbytes < 0)
+  {
+    ssh_channel_close(channel);
+    ssh_channel_free(channel);
+    return SSH_ERROR;
+  }
+ 
+  ssh_channel_send_eof(channel);
+  ssh_channel_close(channel);
+  ssh_channel_free(channel);
+ 
+  return SSH_OK;
 }
